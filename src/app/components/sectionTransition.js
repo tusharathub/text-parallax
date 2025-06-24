65031552bc8081bdb7f9c298190eee728dd161fb
Tushar <tushar.nyc20@gmail.com>
2025-06-24 15:27:15 +0530

    added section transition

---
+++ src/app/components/sectionTransition.js
@@ -0,0 +1,77 @@
+'use client';
+import Image from "next/image";
+import Pic1 from '../../../public/images/1.jpg'
+import Pic2 from '../../../public/images/2.jpg'
+import { useScroll, useTransform, motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Lenis from 'lenis';
+
+export default function SectionTransition() {
+
+  const container = useRef();
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"]
+  }) 
+
+  useEffect( () => {
+    const lenis = new Lenis()
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
+  return (
+    <main ref={container} className="relative h-[200vh]">
+        
+      <Section1 scrollYProgress={scrollYProgress}/>
+      <Section2 scrollYProgress={scrollYProgress}/>
+    </main>
+  );
+}
+
+const Section1 = ({scrollYProgress}) => {
+
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5])
+  return (
+    
+    <motion.div style={{scale, rotate}} className="sticky top-0 h-screen bg-[#C72626] text-[3.5vw] flex flex-col items-center justify-center text-white pb-[10vh]">
+        
+      <p>Scroll Perspective</p>
+      <div className="flex gap-4">
+        <p>Section</p>
+        <div className="relative w-[12.5vw]">
+          <Image 
+            src={Pic1}
+            alt="img"
+            placeholder="blur"
+            fill
+          />
+        </div>
+        <p>Transition</p>
+      </div>
+    </motion.div>
+  )
+}
+
+const Section2 = ({scrollYProgress}) => {
+
+  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [5, 0])
+
+  return (
+    <motion.div style={{scale, rotate}} className="relative h-screen">
+      <Image 
+        src={Pic2}
+        alt="img"
+        placeholder="blur"
+        fill
+      />
+    </motion.div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,67 +1,11 @@
-
-'use client'
-import { useScroll, useTransform, motion } from 'framer-motion';
-import Picture1 from '../../public/images/5.jpg'
-import Picture2 from '../../public/images/8.jpg'
-import Picture3 from '../../public/images/12.jpg'
-import Lenis from 'lenis';
-
-import Image from 'next/image';
-import { useEffect, useRef } from 'react';
+import SectionTransition from "../app/components/sectionTransition"
+import TextParallax from "../app/components/textParallax"
 
 export default function Home() {
-
-  const container = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ['start end', 'end start']
-  })
-
-  useEffect( () => {
-    const lenis = new Lenis()
-
-    function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
-  }, [])
-
-  return (
-    <main className="overflow-hidden">
-      <div className='h-[100vh]'/>
-      <div ref={container} className='bg-gray-100'>
-        <Slide src={Picture1} direction={'left'} left={"-30%"} progress={scrollYProgress}/>
-        <Slide src={Picture2} direction={'right'} left={"-45%"} progress={scrollYProgress}/>
-        <Slide src={Picture3} direction={'left'}  left={"-85%"} progress={scrollYProgress}/>
-        <Slide src={Picture2} direction={'right'} left={"-5%"} progress={scrollYProgress}/>
-      </div>
-      <div className='h-[100vh]' />
-    </main>
-  );
-}
-
-const Slide = (props) => {
-  const direction = props.direction == 'left' ? -1 : 1;
-  const translateX = useTransform(props.progress, [0, 1], [150 * direction, -150 * direction])
-  return (
-    <motion.div style={{x: translateX, left: props.left}} className="relative flex whitespace-nowrap">
-      <Phrase src={props.src}/>
-      <Phrase src={props.src}/>
-      <Phrase src={props.src}/>
-    </motion.div>
-  )
-}
-
-const Phrase = ({src} ) => {
-
-  return (
-    <div className={'px-5 flex gap-5 items-center'}>
-      <p className='text-[7.5vw]'>안녕하세요</p>
-      <span className="relative h-[7.5vw] aspect-[4/2] rounded-full overflow-hidden">
-        <Image style={{objectFit: "cover"}} src={src} alt="image" fill/>
-      </span>
+  return(
+    <div>
+      <SectionTransition/>
+      <TextParallax/>
     </div>
   )
 }
